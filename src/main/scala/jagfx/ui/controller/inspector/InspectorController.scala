db8082dc21bafd002913eb19887de85c02274ebf
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-04 01:54:35 +0200

    rework inspector panel entirely

--- src/main/scala/jagfx/ui/controller/inspector/InspectorController.scala
+++ src/main/scala/jagfx/ui/controller/inspector/InspectorController.scala
@@ -1,91 +1,97 @@
 package jagfx.ui.controller.inspector
 
+import javafx.scene.Node
+import javafx.scene.control._
 import javafx.scene.layout._
-import javafx.scene.control.Label
 import javafx.geometry.Pos
 import jagfx.ui.viewmodel._
 import jagfx.ui.controller.IController
-import jagfx.ui.components.pane.JagModulationPane
+import jagfx.ui.components.button.JagButton
 
 /** Inspector panel for editing envelope or filter parameters. */
-class InspectorController(viewModel: SynthViewModel) extends IController[VBox]:
-  protected val view = VBox()
-  view.getStyleClass.add("inspector")
-  view.setSpacing(8)
+class InspectorController(viewModel: SynthViewModel)
+    extends IController[ScrollPane]:
+  protected val view = ScrollPane()
+  view.getStyleClass.add("inspector-scroll")
+  view.setFitToWidth(true)
+  view.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER)
+
+  private val content = VBox()
+  content.getStyleClass.add("inspector-content")
+  content.setSpacing(8)
+  view.setContent(content)
 
   private val envInspector = EnvelopeInspector()
   private val fltInspector = FilterInspector()
   private val timingInspector = TimingInspector()
-  private val modulationPane = JagModulationPane()
 
-  private val helpHeader = Label("INFO")
-  helpHeader.getStyleClass.add("help-header")
+  // Sections
+  private val topSection = VBox(8)
+  topSection.setAlignment(Pos.TOP_LEFT)
 
-  private val helpDesc = Label("")
-  helpDesc.getStyleClass.add("help-text")
-  helpDesc.setWrapText(true)
+  private val midSection = VBox(8)
+  midSection.setAlignment(Pos.TOP_LEFT)
 
-  private val helpControls = Label("")
-  helpControls.getStyleClass.add("help-text")
-  helpControls.setWrapText(true)
+  private val infoSection = VBox(8)
+  infoSection.setAlignment(Pos.TOP_LEFT)
 
-  private val topPane = VBox(8)
-  topPane.setAlignment(Pos.TOP_LEFT)
+  // Separators
+  private val sep1 = new Separator()
+  private val sep2 = new Separator()
 
-  private val middlePane = VBox(2)
-  middlePane.setAlignment(Pos.BOTTOM_LEFT)
-  VBox.setVgrow(middlePane, Priority.ALWAYS)
+  // Info Text
+  private val infoLabel = Label()
+  infoLabel.getStyleClass.add("help-text")
+  infoLabel.setWrapText(true)
+  infoSection.getChildren.add(infoLabel)
 
-  // add inspectors to top pane, but hide them initially
-  topPane.getChildren.addAll(envInspector, fltInspector, timingInspector)
-  envInspector.setVisible(false)
-  envInspector.setManaged(false)
-  fltInspector.setVisible(false)
-  fltInspector.setManaged(false)
-  timingInspector.setVisible(false)
-  timingInspector.setManaged(false)
+  // Assemble
+  topSection.getChildren.addAll(envInspector, fltInspector)
+  midSection.getChildren.add(timingInspector)
 
-  middlePane.getChildren.addAll(helpHeader, helpDesc, helpControls)
-  view.getChildren.addAll(topPane, middlePane, modulationPane)
+  content.getChildren.addAll(topSection, sep1, midSection, sep2, infoSection)
 
-  // bind mod pane when tone changes
-  viewModel.activeToneIndexProperty.addListener((_, _, _) =>
-    modulationPane.bind(viewModel.getActiveTone)
-  )
-  modulationPane.bind(viewModel.getActiveTone)
+  // Initial State
+  envInspector.setVisible(false); envInspector.setManaged(false)
+  fltInspector.setVisible(false); fltInspector.setManaged(false)
+  timingInspector.setVisible(false); timingInspector.setManaged(false)
 
-  hide()
+  view.setVisible(false)
+
+  private def show(): Unit =
+    view.setVisible(true)
 
   def bind(envelope: EnvelopeViewModel, title: String, desc: String): Unit =
-    show(envInspector)
+    show()
+
+    envInspector.setVisible(true); envInspector.setManaged(true)
+    fltInspector.setVisible(false); fltInspector.setManaged(false)
+
     envInspector.bind(envelope)
+
+    timingInspector.setVisible(true); timingInspector.setManaged(true)
     timingInspector.bind(viewModel.getActiveTone)
-    timingInspector.setVisible(true)
-    timingInspector.setManaged(true)
-    updateHelp(title, desc, envInspector.getHelpText)
+
+    infoLabel.setText(s"$desc")
 
   def bindFilter(filter: FilterViewModel, title: String, desc: String): Unit =
-    show(fltInspector)
+    show()
+
+    envInspector.setVisible(false); envInspector.setManaged(false)
+    fltInspector.setVisible(true); fltInspector.setManaged(true)
+
     fltInspector.bind(filter)
-    updateHelp(title, desc, fltInspector.getHelpText)
+
+    timingInspector.setVisible(true); timingInspector.setManaged(true)
+    timingInspector.bind(viewModel.getActiveTone)
+
+    infoLabel.setText(s"$desc")
 
   def hide(): Unit =
+    view.setVisible(false)
     envInspector.setVisible(false)
     envInspector.setManaged(false)
     fltInspector.setVisible(false)
     fltInspector.setManaged(false)
     timingInspector.setVisible(false)
     timingInspector.setManaged(false)
-    helpHeader.setVisible(false)
-    helpDesc.setText("")
-    helpControls.setText("")
-
-  private def show(node: javafx.scene.Node): Unit =
-    hide()
-    node.setVisible(true)
-    node.setManaged(true)
-    helpHeader.setVisible(true)
-
-  private def updateHelp(title: String, desc: String, controls: String): Unit =
-    helpDesc.setText(desc)
-    helpControls.setText(s"\n$controls")
